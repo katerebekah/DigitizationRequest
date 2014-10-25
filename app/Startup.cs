3fb58cc7459a4698aa1fb7eb3bda0d733697731d
Kate Williams <katerebekah@gmail.com>
2014-10-24 22:09:37 -0500

    Created app

---
+++ app/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(app.Startup))]
+namespace app
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
