1e83e3232cdfb3ceaf3c7a337086fcc98dd9dbc3
Kate Williams <katerebekah@gmail.com>
2014-10-24 22:54:40 -0500

    Successful migration

--- TSLAEntities/Migrations/Configuration.cs
+++ TSLAEntities/Migrations/Configuration.cs
@@ -5,14 +5,14 @@ namespace TSLAEntities.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<TSLAEntities.TSLAEntities>
+    internal sealed class Configuration : DbMigrationsConfiguration<TSLAEntities>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(TSLAEntities.TSLAEntities context)
+        protected override void Seed(TSLAEntities context)
         {
             //  This method will be called after migrating to the latest version.
 
